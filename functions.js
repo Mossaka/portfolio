7c27017b0fb6c7866774f30f5ea019988f5bb97c
Mossaka <jiz417@ucsd.edu>
2017-04-15 11:40:40 -0700

    add introduction add fadeout effect to the header

--- functions.js
+++ functions.js
@@ -2,6 +2,11 @@ $(window).scroll(function () {
     var $wScroll = $(this).scrollTop();
 
     $(".intro-message").css({
-        'transform' : 'translate(0px, ' + $wScroll /30 +'%)'
+        'transform' : 'translate(0px, ' + $wScroll /10+'%)'
     })
+    if($wScroll > 550) {
+        $(".intro-message").fadeOut("fast");
+    } else {
+        $(".intro-message").fadeIn("fast");
+    }
 })
